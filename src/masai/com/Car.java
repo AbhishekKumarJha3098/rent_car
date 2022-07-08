1ca1c3784bc89915bfca32da00ae2b9fff6e8b52
Abhishek Kumar Jha <87441996+AbhishekKumarJha3098@users.noreply.github.com>
2022-07-08 12:21:21 +0530

    Update Car.java

--- src/masai/com/Car.java
+++ src/masai/com/Car.java
@@ -1,5 +1,63 @@
 package masai.com;
 
+import java.util.ArrayList;
+
 public class Car {
+	
+	private int ID;
+    private String Maker, Name, Colour, Type;
+    int SeatingCapacity;
+    String Model, Condition, RegNo;
+    private int RentPerHour;
+    private CarOwner carOwner;
+    
+    // Costructor
+    // Getter & Setter
+    // toString
+    
+    public void Add() {
+        ArrayList<Car> car = Car.View();
+        
+    }
+    
+    
+    public void Update() {
+        ArrayList<Car> car = Car.View();
+    }
+    
+    public void Remove() {
+        ArrayList<Car> car = Car.View();
+    }
+    
+    public static ArrayList<Car> SearchByName(String name) {
+        
+    }
+    
+    public static Car SearchByID(int id) {
+        
+    }
+    
+    public static Car SearchByRegNo(String regNo) {
+        
+    }
+    
+    
+    public static ArrayList<Car> View() {
+        
+    }
+    
+    public static boolean isNameValid(String Name) {
+        
+    }
+
+
+    public static boolean isRegNoValid(String RegNo) {
+        
+    }
+    
+    public boolean isRented() {
+        
+    }
+    
 
 }
