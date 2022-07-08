11b9813bb05ef06d191a5a4716a6ad772d0082fc
Abhishek Kumar Jha <87441996+AbhishekKumarJha3098@users.noreply.github.com>
2022-07-08 12:22:18 +0530

    Update CarOwner.java

--- src/masai/com/CarOwner.java
+++ src/masai/com/CarOwner.java
@@ -1,5 +1,63 @@
 package masai.com;
 
-public class CarOwner {
+import java.io.Serializable;
+import java.util.ArrayList;
 
+public class CarOwner extends Person implements Serializable{
+	
+	private int Balance; // increases after every HOUR when Owner's car(s) is booked
+
+	
+	// Costructor
+	// Getter and Setter
+	// toString
+	
+	
+	
+	@Override
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	 public static ArrayList<CarOwner> SearchByName(String name) {
+	        
+	 }
+	 
+	 public static CarOwner SearchByAadhar(String carOwnerCNIC) {
+	        
+	 }
+	 
+	 public static CarOwner SearchByID(int id) {
+	        
+	 }
+	 
+	 public ArrayList<Car> getAllCars() {
+	        
+	 }
+	
+	
+	public static ArrayList<CarOwner> View() {
+        
+    }
+	
+	
 }
